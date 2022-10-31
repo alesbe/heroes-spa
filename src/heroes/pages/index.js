970cc6e723d36522731ba97c5a469994f62342af
alesbe <aesparzabellver@gmail.com>
2022-11-01 00:34:47 +0100

    added navbar and organized in barrel exports

---
+++ src/heroes/pages/index.js
@@ -0,0 +1,2 @@
+export * from './DcPage'
+export * from './MarvelPage'
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 
-import { LoginPage } from "../auth/pages/LoginPage"
-import { DcPage } from "../heroes/pages/DcPage"
-import { MarvelPage } from "../heroes/pages/MarvelPage"
+import { Navbar } from "../ui"
+
+import { LoginPage } from "../auth"
+import { DcPage, MarvelPage } from "../heroes"
 
 export const AppRouter = () => {
   return (
     <>
+        <Navbar/>
+
         <Routes>
             <Route path="/" element={<Navigate to="/marvel" />} />
 
