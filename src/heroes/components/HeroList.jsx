a43ee6d0e3103326baa51f66579d64ca6afb63e9
alesbe <aesparzabellver@gmail.com>
2022-11-01 20:10:31 +0100

    implemented useMemo when getting heroes from db

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,9 +1,10 @@
+import { useMemo } from 'react';
 import { HeroCard } from '.';
 import { getHeroesByPublisher } from '../helpers'
 
 export const HeroeList = ({publisher}) => {
 
-    const heroes = getHeroesByPublisher( publisher );
+    const heroes = useMemo(() => getHeroesByPublisher( publisher ), [publisher]);
     
     return (
         <div className='row rows-cols-1 row-cols-md-3 g-3'>
