b59966810152acb01d515d4ca928443242e7e99a
alesbe <aesparzabellver@gmail.com>
2022-11-02 00:05:14 +0100

    added search page

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,5 +1,68 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string';
+
+import { useForm } from '../../hooks/useForm'
+import { HeroCard } from '../components'
+
 export const SearchPage = () => {
-  return (
-    <h1>SearchPage</h1>
-  )
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    
+    const { q = '' } = queryString.parse( location.search );
+
+    const { searchText, onInputChange } = useForm({
+        searchText: ''
+    });
+
+    const onSearchSubmit = (event) => {
+        event.preventDefault();
+        if ( searchText.trim().length <= 1 ) return;
+
+        navigate(`?q=${ searchText.toLowerCase().trim() }`);
+    }
+
+    return (
+        <>
+            <h1>Search</h1>
+            <hr/>
+
+            <div className="row">
+            <div className="col-5">
+                <h4>Searching</h4>
+                <hr/>
+                <form onSubmit={ onSearchSubmit }>
+                    <input
+                        type='text'
+                        placeholder='Search a hero'
+                        className='form-control'
+                        name='searchText'
+                        autoComplete='off'
+                        value={ searchText }
+                        onChange={ onInputChange }
+                    />
+                    <button className="btn btn-outline-primary mt-1">
+                        Search
+                    </button>
+                </form>
+            </div>
+
+            <div className="col-7">
+                <h4>Results</h4>
+                <hr/>
+
+                <div className="alert alert-primary">
+                    Search a hero
+                </div>
+
+                <div className="alert alert-danger">
+                    No hero with <b>{ q }</b>
+                </div>
+
+                {/* <HeroCard/> */}
+
+            </div>
+            </div>
+        </>
+    )
 }
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -32,6 +32,13 @@ export const Navbar = () => {
                     >
                         DC
                     </NavLink>
+
+                    <NavLink
+                        className={ ({isActive}) => `nav-item nav-link ${ isActive ? 'active' : '' }` }
+                        to="/search"
+                    >
+                        Search
+                    </NavLink>
                 </div>
             </div>
 
